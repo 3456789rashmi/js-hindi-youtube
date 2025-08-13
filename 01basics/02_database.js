6dff422ebd6caa33a65647e1695c9bfb62db3d67
Rashmi <149098275+3456789rashmi@users.noreply.github.com>
2025-08-13 06:48:32 +0000

    datatypessummary

--- 01basics/02_database.js
+++ 01basics/02_database.js
@@ -1,7 +1,10 @@
-let somenum = 33;
-let stringNumber = String(somenum);
-console.log(stringNumber);
-console.log(typeof stringNumber);
-let value = 3;
-let negValue = -value;
-console.log(negvalue);
+// let somenum = 33;
+// let stringNumber = String(somenum);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// let value = 3;
+// let negValue = -value;
+// console.log(negvalue);
+const id = Symbol('123');
+const another = Symbol('123');
+console.log(id===another);
