bc3ad0d7911dbf33875a0b7fd3b29510f182c6d7
Rashmi <149098275+3456789rashmi@users.noreply.github.com>
2025-08-16 15:17:45 +0000

    scopes

---
+++ 03basics/01_function.js
@@ -0,0 +1,4 @@
+function login(user1){
+    return (`${user1} have logged in`);
+}
+login("Rashmi");
---
+++ 03basics/02_scopes.js
@@ -0,0 +1,18 @@
+function one(){
+    const username = "Rashmi"
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    two();
+}
+one();
+if(true){
+    const username = "Rashmi";
+    if(username === "Rashmi"){
+        const website = "Youtube"
+        console.log(username+website);
+    }
+    console.log(website);
+}
+console.log(username);
