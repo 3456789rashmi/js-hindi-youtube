733852ff810e82ce850cad65b92cb77ba002655b
Rashmi <149098275+3456789rashmi@users.noreply.github.com>
2025-08-16 18:20:53 +0000

    map

---
+++ 04controlflow/two.js
@@ -0,0 +1,26 @@
+// const arr = [1,2,3,4,5]
+// for(const num of arr){
+//     console.log(num);
+// }
+// const greetings = "Hello World!";
+// for(const greet of greetings){
+//     console.log(`Each chat is ${greet}`)
+// }
+//Maps
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United States of America")
+// map.set('Fr',"France")
+// // console.log(map)
+// for(const [key,value] of map){
+//     console.log(key, ':-',value);
+// }
+const myobj = {
+    js : 'javascript',
+    cpp:'c++',
+    rb:"ruby",
+    swift:"swift by apple"
+}
+for  (const key in myobj) {
+    
+}
